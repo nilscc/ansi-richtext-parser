55a14e1a2d0befb3b42bae86afe99acbf1d5c36f
nils.cc <mail@nils.cc>
2024-05-06 23:43:09 +0200

    WIP Parser

--- lib/ansi_richtext_parser.dart
+++ lib/ansi_richtext_parser.dart
@@ -1,7 +1,88 @@
 library ansi_richtext_parser;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/widgets.dart';
+import 'package:petitparser/core.dart';
+import 'package:petitparser/definition.dart';
+import 'package:petitparser/parser.dart';
+
+class AnsiColor {
+  int fgColor;
+  int? bgColor;
+
+  // bool verify = true; // verify range of color numbers
+
+  AnsiColor(this.fgColor, {this.bgColor});
+
+  @override
+  String toString() => 'AnsiColor{fgColor: $fgColor, bgColor: $bgColor}';
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is AnsiColor &&
+          fgColor == other.fgColor &&
+          bgColor == other.bgColor;
+
+  @override
+  int get hashCode => fgColor.hashCode + bgColor.hashCode;
+
+  Color? color() {
+    return null;
+  }
+}
+
+class AnsiParser extends GrammarDefinition {
+  @override
+  Parser start() => ref0(elem).plus();
+
+  Parser elem() => ref0(color) | ref0(text);
+
+  Parser color() => (fgColor() & bgColor())
+      .skip(before: escape(), after: char('m'))
+      .map((values) => AnsiColor(values[0], bgColor: values[1]));
+
+  Parser escape() => string('\x1B[');
+  Parser fgColor() => ref0(integer);
+  Parser bgColor() => ref0(integer).skip(before: char(';')).optional();
+
+  Parser integer() => digit().plus().flatten().map(int.parse);
+
+  Parser notEscape() => escape().not() & any();
+  Parser text() => ref0(notEscape).plus().flatten();
+}
+
+class AnsiColorscheme {
+  Color? ansi(final AnsiColor ansiColor) {
+    return null;
+  }
+}
+
+InlineSpan? _buildInlineSpan(values, colorscheme) {
+  toStyle(AnsiColor ansiColor) => TextStyle(color: colorscheme.ansi(ansiColor));
+
+  return switch (values) {
+    [] => null,
+    [String a] => TextSpan(text: a),
+    [AnsiColor ansiColor, String text] => TextSpan(
+        style: toStyle(ansiColor),
+        text: text,
+      ),
+    _ => null,
+  };
+}
+
+Text parse(String ansi, AnsiColorscheme colorscheme) {
+  final parser = AnsiParser().build();
+  final result = parser.parse(ansi);
+
+  if (result is Failure) {
+    throw result;
+  }
+
+  // check if there's only plain text
+  if (result.value.length == 1 && result.value[0] is String) {
+    return Text(result.value[0]);
+  } else {
+    return Text.rich(_buildInlineSpan(result.value, colorscheme)!);
+  }
 }
--- test/ansi_richtext_parser_test.dart
+++ test/ansi_richtext_parser_test.dart
@@ -1,12 +1,84 @@
+import 'dart:ffi';
+import 'dart:math';
+
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:ansi_richtext_parser/ansi_richtext_parser.dart';
+import 'package:petitparser/core.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  final colorscheme = AnsiColorscheme();
+
+  test('Parse plain text', () {
+    const input = 'asdkj238sdknvsdkjwskef';
+    final output = parse(input, colorscheme);
+    expect(output.data, equals(input));
+  });
+
+  // group(("Parser tests"), () {
+  // test("Color parser", () {
+  //   // helper function
+  //   parse(val) => AnsiColor().build().parse(val);
+
+  //   expect(parse('\x1B[30m').value, equals([30, null]));
+  //   expect(parse('\x1B[30;40m').value, equals([30, 40]));
+  //   expect(parse('\x1B[50;200m') is Failure, isTrue);
+  //   expect(parse('\x1B[0;0m') is Failure, isTrue);
+  //   expect(parse('\x1B[96m').value, equals([96, null]));
+  //   expect(parse('\x1B[96;102m').value, equals([96, 102]));
+  //   expect(parse('\x1B[;102m') is Failure, isTrue);
+  //   expect(parse('\x1B[96;m') is Failure, isTrue);
+  // });
+
+  group("Ansi parser", () {
+    final parser = AnsiParser().build();
+    parse(val) => parser.parse(val);
+
+    test('No colors', () {
+      final result = parse("abc");
+      expect(result is Success, isTrue);
+      expect(result.value.length, equals(1));
+      expect(result.value[0], equals("abc"));
+    });
+
+    test('FG color', () {
+      final result = parse("\x1B[30m");
+      expect(result is Success, isTrue);
+      expect(result.value.length, equals(1));
+      expect(result.value[0], equals(AnsiColor(30)));
+    });
+
+    test('FG + BG color', () {
+      final result = parse("\x1B[30;40m");
+      expect(result is Success, isTrue);
+      expect(result.value.length, equals(1));
+      expect(result.value[0], equals(AnsiColor(30, bgColor: 40)));
+    });
+
+    test('Mixed colors and text', () {
+      final result = parse("\u001b[96m29/\u001b[36m40");
+      expect(result is Success, isTrue);
+      expect(result.value.length, equals(4));
+      expect(result.value[0], equals(AnsiColor(96))); // first token
+      expect(result.value[1], equals("29/")); // first string
+      expect(result.value[2], equals(AnsiColor(36))); // second token
+      expect(result.value[3], equals("40")); // second string
+    });
+  });
+
+  group("Complex inputs", () {
+    const raw_input =
+        "\u001b[96m29/\u001b[36m40 | Filter: \u001b[93mrelay | Sort: \u001b[97m~name";
+
+    test(
+      "Get plain text, ignore color information",
+      skip: true, // TODO...
+      () {
+        final output = parse(raw_input, colorscheme);
+        expect(output.textSpan!.toPlainText(),
+            "29/36 | Filter: relay | Sort: name");
+      },
+    );
   });
 }
